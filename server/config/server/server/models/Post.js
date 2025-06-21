a6f45fabb276dcee7a90c2c629b7d228237fbc6e
Zinhle <167529137+NomathembaZinhle@users.noreply.github.com>
2025-06-21 19:55:41 +0200

    Create Post.js

---
+++ server/config/server/server/models/Post.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: String,
+  image: String,
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model('Post', postSchema);
